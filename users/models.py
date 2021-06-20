8210efb6eb3f90d10dc7a8a90ce38748c313cfc8
Riyomi <sz.richard98@gmail.com>
2021-06-20 14:31:54 +0200

    Updated Customer model

--- users/models.py
+++ users/models.py
@@ -1,5 +1,12 @@
 from django.db import models
+from django import forms
 
 
 class Customer(models.Model):
-    pass
+    email = models.EmailField('Email')
+    password = models.CharField(widget=forms.PasswordInput, max_length=30)
+    name = models.CharField(max_length=100)
+    mobile = models.CharField(max_length=20)
+    country = models.CharField(max_length=50)
+    city = models.CharField(max_length=30)
+    address = models.CharField(max_length=50)
