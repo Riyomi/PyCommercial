22731e64deaeea65cc23116eb8273cef51f62aab
Riyomi <sz.richard98@gmail.com>
2021-06-21 22:13:26 +0200

    Clear up sign up form

--- users/forms.py
+++ users/forms.py
@@ -7,7 +7,7 @@ from .models import Customer
 class UserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'email', 'password1', 'password2', 'first_name', 'last_name')
 
 
 class CustomerForm(forms.ModelForm):
