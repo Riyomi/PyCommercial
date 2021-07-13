1b825beba6f9d4cc42a6f6a3325b47f636507be6
Riyomi <sz.richard98@gmail.com>
2021-07-13 21:23:22 +0200

    Fixes #6

--- products/static/js/checkboxList.js
+++ products/static/js/checkboxList.js
@@ -1,38 +1,63 @@
 $(document).ready(function () {
+  // add event listener to every checkbox
   $("#category-selection input[type=checkbox]").change(function () {
-    const currentCheckbox = $(this);
+    const checkbox = $(this);
+    const parents = getParents(checkbox);
 
-    const siblings = currentCheckbox
-      .parents("ul")
-      .siblings('input[type="checkbox"]');
+    checkChildren(checkbox);
 
-    // checks & unchecks parents
-    $(siblings).each(function () {
-      if (!uncheckParents(currentCheckbox)) {
-        $(this).prop("checked", currentCheckbox.prop("checked"));
-      }
-    });
-
-    // Checks & unchecks all children
-    currentCheckbox
-      .siblings("ul")
-      .find('input[type="checkbox"]')
-      .prop("checked", currentCheckbox.prop("checked"));
+    // if the checkbox got checked, recursively check all parents
+    if (isChecked(checkbox)) {
+      $(parents).each(function (_, parent) {
+        check(parent);
+      });
+    } else {
+      // if the checkbox got unchecked, only uncheck a parent if none of its children are checked
+      $(parents).each(function (index, parent) {
+        // the top category (=category without parents, which is always the first element in the array) must be handled differently because of the way the HTML is structured
+        if (index == 0) {
+          if (noSiblingsSelectedOfRoot(parent)) {
+            uncheck(parent);
+          }
+        } else if (noSiblingsSelected(checkbox)) {
+          uncheck(parent);
+        }
+      });
+    }
   });
 });
 
-function uncheckParents(checkbox) {
-  // if at least one sibling is checked
-  // or any of the siblings of the parent is checked
-  // no need to uncheck the parent
+function getParents(checkbox) {
+  return checkbox.parents("ul").siblings('input[type="checkbox"]');
+}
 
-  console.log();
+function checkChildren(checkbox) {
+  checkbox
+    .siblings("ul")
+    .find('input[type="checkbox"]')
+    .prop("checked", checkbox.prop("checked"));
+}
+
+function noSiblingsSelected(checkbox) {
   return (
-    checkbox.closest("li").siblings().children("input:checked").length > 0 ||
-    checkbox
-      .closest("ul")
-      .parents("ul")
-      .children("li")
-      .children("input:checked").length > 0
+    checkbox.closest("li").siblings().children("input:checked").length == 0
   );
+}
+
+function noSiblingsSelectedOfRoot(root) {
+  return (
+    $(root).siblings("ul").children("li").children("input:checked").length == 0
+  );
+}
+
+function isChecked(checkbox) {
+  return checkbox.prop("checked");
+}
+
+function check(checkbox) {
+  $(checkbox).prop("checked", true);
+}
+
+function uncheck(checkbox) {
+  $(checkbox).prop("checked", false);
 }
