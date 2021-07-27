cb45f75cf79e751ff100afc661d7b98d8173492a
Riyomi <sz.richard98@gmail.com>
2021-07-27 22:29:07 +0200

    Orders page, Order details page and Cart responsive design added

--- static/js/dropdown.js
+++ static/js/dropdown.js
@@ -1,3 +1,23 @@
+$("#mobiledropdown").on("click", function () {
+  $("#mobileDropDownMenu").toggleClass("hidden");
+
+  if (!$("#mobileDropDownMenu").hasClass("hidden")) {
+    $("#menuIcon").attr("d", "M6 18L18 6M6 6l12 12");
+  } else {
+    $("#menuIcon").attr("d", "M4 6h16M4 12h16M4 18h16");
+  }
+});
+
+$("#mobileaccountdropdown").on("click", function () {
+  $("#mobileAccountDropDown").toggleClass("hidden");
+
+  if (!$("#mobileAccountDropDown").hasClass("hidden")) {
+    $("#accountMenuIcon").attr("d", "M6 18L18 6M6 6l12 12");
+  } else {
+    $("#accountMenuIcon").attr("d", "M4 6h16M4 12h16M4 18h16");
+  }
+});
+
 function openMenu(id) {
   $(`#${id}`).next().toggleClass("hidden");
 }
