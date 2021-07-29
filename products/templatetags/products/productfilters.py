ded383fae100df59766d78883f5c3903ccf15984
Riyomi <sz.richard98@gmail.com>
2021-07-29 21:43:14 +0200

    .

--- products/templatetags/products/productfilters.py
+++ products/templatetags/products/productfilters.py
@@ -5,12 +5,6 @@ from math import floor, ceil
 register = template.Library()
 
 
-@register.filter(name='formaturl')
-def format_url(url):
-    stripped = url.split('/')[2:]
-    return '/' + '/'.join(stripped)
-
-
 @register.filter(name='truncatetext')
 def truncate_text(text, param):
     return (text[:param] + '..') if len(text) > param else text
